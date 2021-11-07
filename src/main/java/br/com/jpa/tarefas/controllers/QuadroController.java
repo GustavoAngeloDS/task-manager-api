502ac04e19a95de298b646de3be029b8758ae2fb
Gustavo Angelo da Silva <gustavosilvaangelo@gmail.com>
2021-11-07 16:25:06 -0300

    Quadro delete mapping adicionado

--- src/main/java/br/com/jpa/tarefas/controllers/QuadroController.java
+++ src/main/java/br/com/jpa/tarefas/controllers/QuadroController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,11 @@ public class QuadroController {
 	@PutMapping
 	public ResponseEntity<Quadro> update(@RequestBody Quadro quadro) {
 		return new ResponseEntity<>(quadroService.update(quadro), HttpStatus.OK);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> remove(@RequestBody Quadro quadro){
+		quadroService.remove(quadro);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
--- src/main/java/br/com/jpa/tarefas/services/QuadroService.java
+++ src/main/java/br/com/jpa/tarefas/services/QuadroService.java
@@ -3,6 +3,8 @@ package br.com.jpa.tarefas.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,26 +14,33 @@ import br.com.jpa.tarefas.repositories.QuadroRepository;
 @Service
 public class QuadroService {
 	
+	Logger logger = LoggerFactory.getLogger(QuadroService.class);
+	
 	@Autowired
 	QuadroRepository quadroRepository;
 	
 	public List<Quadro> findAll(){
+		logger.info("Retornando informações de todos os quadros");
 		return quadroRepository.findAll();
 	}
 	
 	public Quadro findById(Integer id) {
+		logger.info("Retornando informações de quadro por ID");
 		return ((Optional<Quadro>) quadroRepository.findById(id)).get();
 	}
 	
 	public Quadro save(Quadro quadro) {
+		logger.info("Salvando novo quadro");
 		return quadroRepository.save(quadro);
 	}
 	
 	public void remove(Quadro quadro) {
+		logger.info("Removendo quadro");
 		quadroRepository.delete(quadro);
 	}
 	
 	public Quadro update(Quadro quadro) {
+		logger.info("Atualizando quadro");
 		Optional<Quadro> quadroOpt = quadroRepository.findById(quadro.getId());
 		Quadro quadroFinal = new Quadro();
 		
