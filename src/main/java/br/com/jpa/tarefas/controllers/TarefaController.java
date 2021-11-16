7416edbb2dc1464c8ed1f88fca70d3e247fa00fb
Gustavo Angelo da Silva <gustavosilvaangelo@gmail.com>
2021-11-16 16:57:37 -0300

    Endpoint para deletar tarefas

--- src/main/java/br/com/jpa/tarefas/controllers/TarefaController.java
+++ src/main/java/br/com/jpa/tarefas/controllers/TarefaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,11 @@ public class TarefaController {
 	@PutMapping
 	public ResponseEntity<Tarefa> update(@RequestBody Tarefa tarefa) {
 		return new ResponseEntity<>(tarefaService.update(tarefa), HttpStatus.OK);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> remove(@RequestBody Tarefa tarefa){
+		tarefaService.remove(tarefa);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
